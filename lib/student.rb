2b74914326e6d90e7096f3f5538121987f56f2f4
Richard Yance <yance0022@gmail.com>
2019-01-15 02:50:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
 class Student < User
+  
+    def initialize
+      @knowledge = []
+    end
+
+    def learn(string)
+      @knowledge << "Ruby framework Rails gem bundle update"
+    end
+
+    def knowledge
+    @knowledge << "Javascript Ember Elixir knowledge"
+    end
+  end
 
 end
